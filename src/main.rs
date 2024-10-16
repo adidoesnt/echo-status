ee15df029c52de734f293ba10eef3f42f7c82471
Banerjee Aditya <aditya.banerjee@activate.sg>
2024-10-16 18:01:25 +0800

    add echo status implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use axum::{routing::get, Router};
+use axum::{
+    extract::Query,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
 use dotenv::dotenv;
+use std::collections::HashMap;
 use std::env;
 
 #[tokio::main]
@@ -7,7 +13,7 @@ async fn main() {
     println!("Loading environment variables...");
     dotenv().ok();
 
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(echo_status));
 
     let host = env::var("HOST").unwrap_or("127.0.0.1".to_string());
     let port = env::var("PORT").unwrap_or("12007".to_string());
@@ -16,4 +22,16 @@ async fn main() {
     println!("Starting server on {}", addr);
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn echo_status(Query(params): Query<HashMap<String, String>>) -> impl IntoResponse {
+    println!("GET /echo_status");
+    let default: String = "200".to_string();
+    let status: u16 = params.get("status").unwrap_or(&default).parse().unwrap();
+    println!("GET /echo_status::Received status {}", status);
+
+    Response::builder()
+        .status(status)
+        .body(status.to_string())
+        .unwrap()
 }
