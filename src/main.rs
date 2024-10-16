95a071dfad135fc715c24d45ceb2a09ecc7c7237
Banerjee Aditya <aditya.banerjee@activate.sg>
2024-10-16 11:14:00 +0800

    add hello world route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use dotenv::dotenv;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    println!("Loading environment variables...");
+    dotenv().ok();
+
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let host = env::var("HOST").unwrap_or("127.0.0.1".to_string());
+    let port = env::var("PORT").unwrap_or("12007".to_string());
+    let addr = format!("{}:{}", host, port);
+
+    println!("Starting server on {}", addr);
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
